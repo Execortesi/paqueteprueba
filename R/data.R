3e4aec5e408ec74cf896a4bbdb0392a010e051c8
Execortesi <execonerc@gmail.com>
2024-10-04 10:03:51 -0300

    Agrego cambios

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Conjunto de datos de ejemplo
+#'
+#' Los datos son utilizados como ejemplo de muestra de función de suma.
+#'
+#' @format los datos son generados de forma sintética.
+#' \describe{
+#'  \item{a}{muestra de datos}
+#'  \item{b}{muestra de datos multiplicada por cinco}
+#' }
+#' @source generación propia con funciones de R
+"datos"
+
