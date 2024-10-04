1aebd0726cd22df351f939782da293082bf4dcce
Execortesi <execonerc@gmail.com>
2024-10-04 09:32:33 -0300

    A

--- R/suma.R
+++ R/suma.R
@@ -1,12 +1,22 @@
-# Suma dos números
-
+#' Suma dos numeros
+#'
+#' La función `suma()´ toma dos números positivos y los suma.
+#'
+#' Estos son detalles extras de la función.
+#'
+#' @param x un vector numérico positivo
+#' @param y un vector numérico positivo
+#'
+#' @return
+#' Un vector numerico con la suma de `x´ e `y´.
+#'
+#' @examples
+#' suma(2, 2)
+#' suma(1, 10)
+#'
+#' @export
 suma <- function(x = 0, y = 0) {
 
   return(x + y)
 }
-suma2 <- function(x, y){
-  if (!is.numeric(x) | !is.numeric(y)) {
-    return("Error: Ambos argumentos deben ser numéricos.")
-  }
-  return(x + y)
-}
+
